8c4d974572397b948b5847762b3343e092f11656
Rishu <shrivastava.rishu5@gmail.com>
2024-09-22 13:30:07 +0530

    Email Service

---
+++ src/main/java/org/userservice/emailservice/dtos/EmailDto.java
@@ -0,0 +1,12 @@
+package org.userservice.emailservice.dtos;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class EmailDto {
+    private String to;
+    private String from;
+    private String subject;
+    private String body;
+}
